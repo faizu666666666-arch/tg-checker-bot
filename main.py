5cd9225a1fa0ad8c1d9e0ab535df84b714956dac
faizu666666666-arch <faizu666666666@gmail.com>
2026-07-11 18:56:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,22 @@ with client:
     with client:
     client.loop.run_until_complete(main())
     client.run_until_disconnected()
+    import os
+from telethon.sync import TelegramClient
+
+api_id = os.environ.get('API_ID')
+api_hash = os.environ.get('API_HASH')
+string_session = os.environ.get('STRING_SESSION')
+
+client = TelegramClient(string_session, api_id, api_hash)
+
+async def main():
+    me = await client.get_me()
+    print(f"Bot logged in as: {me.first_name}")
+
+with client:
+    client.loop.run_until_complete(main())
+    client.run_until_disconnected() # This keeps the bot alive!
+    
     
   
